3135980fc6225fc1bc69626667b06bb3e5685509
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-05-27 17:14:29 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,5 +3,15 @@ $(function(){
     $('.main__slaider').slick({
         dots: true,
         arrows: false
-        });
+    });
+    
+    $('.popup-with-form').magnificPopup({
+   type: 'inline',
+   focus: '#name'
+    });
+    
+    $('#close').click(function(){
+        $('.mfp-close').trigger('click');
+      });
+
 });
